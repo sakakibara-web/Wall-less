bd56349f08af7e5e073e0851f66fe3c90cbe1627
Kumiko Sakakibara <shenyuanjiumeizi1@gmail.com>
2025-09-26 15:20:25 +0900

    Cleaned up project files and ready for push

--- Library/PackageCache/com.unity.addressables.android/Editor/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyCompany("Unity Technologies")]
-
-[assembly: InternalsVisibleTo("Unity.Addressables.Android.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.Addressables.Android.Base.Tests")]
-[assembly: InternalsVisibleTo("Unity.Addressables.Android.Tests")]
